04f2e63827979c85998bc200f5e68de614a84bd4
Prajyotful <prajyot.sonu14@gmail.com>
2019-02-23 11:19:43 +0000

    add string

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-	System.out.println(DateTime.now());
+        System.out.println("Hello, World!");
+       String t="Welcome Prajyot Fulsundar To  Devops World !!!!!!!!!!!!!!!!!";	
+	System.out.println(t);
     }
 
 }
